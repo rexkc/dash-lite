0ab974414d95e562264a4744e3b925968e6dbf0a
rexkc <rexkc1028@gmail.com>
2018-06-24 22:35:21 +1000

    local working version of floorplan

---
+++ functions.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import json
+from flask import request
+from collections import Counter
+from vars import token
+
+def requestHeaders():
+    try:
+        int(os.getenv("VCAP_APP_PORT"))!= 5000 #app is running in cloud foundry
+        headers =  {'Authorization': request.headers["Authorization"]}
+    except:
+        headers = {'Authorization': 'Bearer ' + token['access_token']}
+    return headers
+
+def locationMapping():
+    return
+
+def plotts(data,var):
+    plotdata = []
+    plottime = []
+    for i in range(len(data)):
+        plotdata.append(data[i][var])
+        plottime.append(str(data[i]['_time']))
+    print(plottime)
+    return plotdata, plottime
+
+def populateFloorplan(Asset,Aspect,dateFrom,dateTo):
+    # create request url
+    url="https://gateway.eu1.mindsphere.io/api/iottimeseries/v3/timeseries/{0}/{1}?from={2}&to={3}".format(Asset,Aspect,dateFrom,dateTo)
+    response =requests.get(url, headers=requestHeaders())
+    # load response in dict
+    raw_dict = json.loads(response.text)
+    # initialize array for returning data
+    dataArray = []
+
+    #TODO make all location codes into array so this can run on a for loop (same for message codes) 
+    bwNth = [x for x in raw_dict if x['locationCode'] == 30102] # filter for B3 Nth Entry Dr Ea
+    counts = Counter(c['messageCode'] for c in bwNth) # get counts of message occurances
+    dataArray.append({'label' : 'B3 Nth Entry Dr Ea', 'x': -0.023, 'y': 0.36, 'PB': counts[17300], 'CR' : counts[17200]}) # add data to array
+
+    bwGnd = [x for x in raw_dict if x['locationCode'] == 30206] # filter for B3 Gnd To Link
+    counts = Counter(c['messageCode'] for c in bwGnd) # get counts of message occurances
+    dataArray.append({'label' : 'B3 Gnd To Link', 'x': 0.76, 'y': -0.2, 'PB': counts[17300], 'CR' : counts[17200]}) # add data to array
+    print(dataArray)
+    return dataArray
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from functions import populateFloorplan, plotts, requestHeaders
 from flask import Flask, render_template, jsonify, url_for, flash, request
 from random import sample
 import os
@@ -10,8 +11,6 @@ import urllib
 import json
 
 app = Flask(__name__)
-
-jwt = 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImtleS1pZC0xIiwidHlwIjoiSldUIn0.eyJqdGkiOiI5YmNjYjEzNzAzODM0MzIxYjMxYWY1ZWFkNTEzOTc5NiIsInN1YiI6InRlY2h1c3IiLCJzY29wZSI6WyJtZHNwOmNvcmU6QWRtaW4zcmRQYXJ0eVRlY2hVc2VyIl0sImNsaWVudF9pZCI6InRlY2h1c3IiLCJjaWQiOiJ0ZWNodXNyIiwiYXpwIjoidGVjaHVzciIsImdyYW50X3R5cGUiOiJjbGllbnRfY3JlZGVudGlhbHMiLCJyZXZfc2lnIjoiZmYwMjU3NTAiLCJpYXQiOjE1Mjk1OTA0NDksImV4cCI6MTUyOTU5MjI0OSwiaXNzIjoiaHR0cHM6Ly9zaWxkZXZtcy5waWFtLmV1MS5taW5kc3BoZXJlLmlvL29hdXRoL3Rva2VuIiwiemlkIjoic2lsZGV2bXMiLCJhdWQiOlsidGVjaHVzciJdLCJ0ZW4iOiJzaWxkZXZtcyIsInNjaGVtYXMiOlsidXJuOnNpZW1lbnM6bWluZHNwaGVyZTppYW06djEiXSwiY2F0IjoiY2xpZW50LXRva2VuOnYxIn0.a2CDi3ge9vliDYie-H1Pw8XJVJ-mqwS6lkr3eaGh5YEfGZkbSeqTPaZ7l9ks7DUji4-nesbO4hmn9d3-owSA6izgwRXuelOmsXX5ErHI0YgmHlWR8-Z16wAsrmXW_HvrpRXIM2Qhdj2xQFrzP0S_u9Io9Qu-5mF5UKsQcB8qHjCvM65SkCbC50d2szSWsT84NZRv0ruHv1V89Y-r9ibKCdpKoQjTi6uH3gJGykW_cO4hm_q_x3tsCxe1ViRPq3z7iQ9DkSnM6V9uQ6GBmYD8_watajX2f2nbEtfXD32Y3LI8wfNkx5wVWixu_W2MYwi7jpYPN0ABrBeiCy_xVM4xxg'
 
 @app.route('/')
 def root():
@@ -28,7 +27,7 @@ def reports():
 
 @app.route('/floorplan')
 def floorplan():
-    dataArray = [{'x': 0, 'y': 0.4, 'v': 900},{'x': -0.9, 'y': 0.4, 'v': 900}]
+    dataArray = populateFloorplan('58c75abe35a24a57a18754170d498f4e','BuildingSecurityAspect','2017-11-30T00:00:00Z','2017-12-07T00:00:00Z')
     return render_template("floorplan.html",dataArray = dataArray)
 
 @app.route('/data')
@@ -38,9 +37,7 @@ def data():
 @app.route('/assets')
 def assets():
     url = 'https://gateway.eu1.mindsphere.io/api/assetmanagement/v3/assets/?size=200'
-    headers = {'Authorization': request.headers["Authorization"]}
-    # headers = {'Authorization': jwt}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=requestHeaders())
     data = json.loads(response.text)
     for i in range(len(data['_embedded']['assets'])):
         if '152' in data['_embedded']['assets'][i]['name']:
@@ -50,30 +47,15 @@ def assets():
 @app.route('/assets/<id>/aspects')
 def assets_aspects(id):
     url = 'https://gateway.eu1.mindsphere.io/api/assetmanagement/v3/assets/{0}/aspects'.format(id)
-    headers = {'Authorization': request.headers["Authorization"]}
-    # headers = {'Authorization': jwt}
-    response =requests.get(url, headers=headers)
+    response =requests.get(url, headers=requestHeaders())
     return render_template('aspects.html', aspects_data = json.loads(response.text))
 
 @app.route("/timeseries/<asset>/<aspect>/<var>", methods=['GET','POST'])
-def timeseries(asset,aspect,var):
-    # params_string = ""
-    # url="https://gateway.eu1.mindsphere.io/api/iottimeseries/v3/timeseries/{0}/{1}{2}".format(asset,aspect,params_string)
+def timeseries(asset,aspect,var):   
     url="https://gateway.eu1.mindsphere.io/api/iottimeseries/v3/timeseries/{0}/{1}?from=2018-06-18T00:00:00Z&to=2018-06-19T23:50:00Z".format(asset,aspect)
-    # headers = {'Authorization': jwt}
-    headers = {'Authorization': request.headers["Authorization"]}
-    response =requests.get(url, headers=headers)
+    response =requests.get(url, headers=requestHeaders())
     (plotdata,plottime) = plotts(json.loads(response.text),var)
     return render_template('timeseries.html',plotdata=plotdata,plottime=plottime)
-
-def plotts(data,var):
-    plotdata = []
-    plottime = []
-    for i in range(len(data)):
-        plotdata.append(data[i][var])
-        plottime.append(str(data[i]['_time']))
-    print(plottime)
-    return plotdata, plottime
 
 #test if env is in cloud foundry by getting VCAP port
 try:
